50142b0eb35dd8b3b4efcf086a52ded21e614a7a
Leonardo Blanski <leo_blanski@hotmail.com>
2024-08-07 19:08:35 -0300

    Add transaction to home

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -4,22 +4,26 @@ namespace App\Filament\Pages;
 
 use App\Filament\Widgets\TransactionChartPieIn;
 use App\Filament\Widgets\TransactionChartPieOut;
-use App\Filament\Widgets\TransactionTotals;
 use App\Filament\Widgets\TransactionTotalStats;
+use App\Models\Transaction;
+use Filament\Actions\CreateAction;
 use Filament\Facades\Filament;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
-use Filament\Forms\Get;
 use Filament\Pages\Dashboard as BaseDashboard;
-use Filament\Pages\Dashboard\Actions\FilterAction;
-use Filament\Pages\Dashboard\Concerns\HasFiltersAction;
 use Filament\Pages\Dashboard\Concerns\HasFiltersForm;
 
 class Dashboard extends BaseDashboard
 {
     use HasFiltersForm;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()->modal()->model(Transaction::class)->form(Transaction::getForm())->slideOver()
+        ];
+    }
 
     public function filtersForm(Form $form): Form
     {
