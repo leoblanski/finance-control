e654c8158630064b90b7cbbc367edfa3a33d8063
Leonardo Blanski <leo_blanski@hotmail.com>
2024-08-08 18:07:46 -0300

    Language provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->circular()
+                ->locales(['pt_BR', 'en_US']);
+        });
     }
 }
